233dda0873805aec546fc6213fc3065759106197
AkhdanHanif19 <hanifakhdan25@gmail.com>
2026-06-30 20:57:38 +0700

    Add files via upload

---
+++ Print.py
@@ -0,0 +1,28 @@
+print("-------------------------------")
+print("Aku Sinau Python")
+print("-------------------------------")
+
+print("PATH : D:\\Akhdan\\Learning Python\\Print.py")
+
+print("this is my code\n")
+print ("this also is my code")
+
+print("this is my code\nthis also is")
+print("this is my code\tthis also is")
+
+
+print("Your Learning Path:\n\t- Python Basics\n\t- Data Engineering\n\t- AI")
+
+print("""Your Learning Path:
+      \t- Python Basics
+      \t- Data Engineering
+      \t- AI\n""")
+
+# Print practice
+price_shirt = 1000
+price_pants = 750
+qty_shirt = 2
+qty_pants = 3
+
+total_price = (price_shirt * qty_shirt) + (price_pants * qty_pants)
+print("Total Price: ", total_price)
---
+++ hello.py
@@ -0,0 +1,6 @@
+print("Akhdan is learning")
+# This is a comment
+
+# Comment is usually put above a code to explain the logic!
+ # or you can put it beside the code
+
